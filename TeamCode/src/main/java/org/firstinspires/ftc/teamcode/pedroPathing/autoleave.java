02be237960b5b6b83d8482f4e2c30f2f6bf8177c
epdu <epdu@temple.edu>
2026-01-31 00:57:28 -0500

    auto finetune

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/autoleave.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/autoleave.java
@@ -1,43 +1,43 @@
-package org.firstinspires.ftc.teamcode.pedroPathing;
-
-
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.teamcode.pedroPathing.HardwareQualifier;
-
-@Autonomous
-public class autoleave extends LinearOpMode {
-
-    public float DriveTrains_ReducePOWER = 0.7f;
-    HardwareQualifier robot = new HardwareQualifier();
-
-    public String fieldOrRobotCentric = "robot";
-    boolean move = false;
-    boolean movementActive = false;
-
-    private volatile boolean isRunning = true;
-    ElapsedTime delayTimer = new ElapsedTime();
-
-
-    @Override
-    public void runOpMode() {
-        robot.init(hardwareMap);
-//        gyro.robot.init(hardwareMap);
-
-        waitForStart();
-
-        robot.leftRearMotor.setPower(-0.6);
-        robot.leftFrontMotor.setPower(-0.6);
-        robot.rightRearMotor.setPower(-0.6);
-        robot.rightFrontMotor.setPower(-0.6);
-        sleep(200);
-        robot.leftRearMotor.setPower(0);
-        robot.leftFrontMotor.setPower(0);
-        robot.rightRearMotor.setPower(0);
-        robot.rightFrontMotor.setPower(0);
-
-        stop();
-    }
-}
+//package org.firstinspires.ftc.teamcode.pedroPathing;
+//
+//
+//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.util.ElapsedTime;
+//
+//import org.firstinspires.ftc.teamcode.pedroPathing.HardwareQualifier;
+//
+//@Autonomous
+//public class autoleave extends LinearOpMode {
+//
+//    public float DriveTrains_ReducePOWER = 0.7f;
+//    HardwareQualifier robot = new HardwareQualifier();
+//
+//    public String fieldOrRobotCentric = "robot";
+//    boolean move = false;
+//    boolean movementActive = false;
+//
+//    private volatile boolean isRunning = true;
+//    ElapsedTime delayTimer = new ElapsedTime();
+//
+//
+//    @Override
+//    public void runOpMode() {
+//        robot.init(hardwareMap);
+////        gyro.robot.init(hardwareMap);
+//
+//        waitForStart();
+//
+//        robot.leftRearMotor.setPower(-0.6);
+//        robot.leftFrontMotor.setPower(-0.6);
+//        robot.rightRearMotor.setPower(-0.6);
+//        robot.rightFrontMotor.setPower(-0.6);
+//        sleep(200);
+//        robot.leftRearMotor.setPower(0);
+//        robot.leftFrontMotor.setPower(0);
+//        robot.rightRearMotor.setPower(0);
+//        robot.rightFrontMotor.setPower(0);
+//
+//        stop();
+//    }
+//}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/rr.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/rr.java
@@ -1,68 +1,68 @@
-package org.firstinspires.ftc.teamcode.pedroPathing;
-
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
-public class rr {
-
-    private DcMotor frontLeft;
-    private DcMotor backLeft;
-    private DcMotor frontRight;
-    private DcMotor backRight;
-
-    public void init(HardwareMap hardwareMap) {
-
-        frontLeft  = hardwareMap.get(DcMotor.class, "front_left_motor");
-        backLeft   = hardwareMap.get(DcMotor.class, "back_left_motor");
-        frontRight = hardwareMap.get(DcMotor.class, "front_right_motor");
-        backRight  = hardwareMap.get(DcMotor.class, "back_right_motor");
-
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
-
-        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-    }
-
-    public void drive(double forward, double strafe, double rotate) {
-
-        double frontLeftPower  = forward + strafe + rotate;
-        double backLeftPower   = forward - strafe + rotate;
-        double frontRightPower = forward - strafe - rotate;
-        double backRightPower  = forward + strafe - rotate;
-
-        double max = Math.max(
-                Math.max(Math.abs(frontLeftPower), Math.abs(backLeftPower)),
-                Math.max(Math.abs(frontRightPower), Math.abs(backRightPower))
-        );
-
-        if (max > 1.0) {
-            frontLeftPower  /= max;
-            backLeftPower   /= max;
-            frontRightPower /= max;
-            backRightPower  /= max;
-        }
-
-        frontLeft.setPower(frontLeftPower);
-        backLeft.setPower(backLeftPower);
-        frontRight.setPower(frontRightPower);
-        backRight.setPower(backRightPower);
-    }
-
-    public void stop() {
-        frontLeft.setPower(0);
-        backLeft.setPower(0);
-        frontRight.setPower(0);
-        backRight.setPower(0);
-    }
-}
+//package org.firstinspires.ftc.teamcode.pedroPathing;
+//
+//import com.qualcomm.robotcore.hardware.DcMotor;
+//import com.qualcomm.robotcore.hardware.DcMotorSimple;
+//import com.qualcomm.robotcore.hardware.HardwareMap;
+//
+//public class rr {
+//
+//    private DcMotor frontLeft;
+//    private DcMotor backLeft;
+//    private DcMotor frontRight;
+//    private DcMotor backRight;
+//
+//    public void init(HardwareMap hardwareMap) {
+//
+//        frontLeft  = hardwareMap.get(DcMotor.class, "front_left_motor");
+//        backLeft   = hardwareMap.get(DcMotor.class, "back_left_motor");
+//        frontRight = hardwareMap.get(DcMotor.class, "front_right_motor");
+//        backRight  = hardwareMap.get(DcMotor.class, "back_right_motor");
+//
+//        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+//        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+//        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+//        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+//
+//        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//
+//        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//    }
+//
+//    public void drive(double forward, double strafe, double rotate) {
+//
+//        double frontLeftPower  = forward + strafe + rotate;
+//        double backLeftPower   = forward - strafe + rotate;
+//        double frontRightPower = forward - strafe - rotate;
+//        double backRightPower  = forward + strafe - rotate;
+//
+//        double max = Math.max(
+//                Math.max(Math.abs(frontLeftPower), Math.abs(backLeftPower)),
+//                Math.max(Math.abs(frontRightPower), Math.abs(backRightPower))
+//        );
+//
+//        if (max > 1.0) {
+//            frontLeftPower  /= max;
+//            backLeftPower   /= max;
+//            frontRightPower /= max;
+//            backRightPower  /= max;
+//        }
+//
+//        frontLeft.setPower(frontLeftPower);
+//        backLeft.setPower(backLeftPower);
+//        frontRight.setPower(frontRightPower);
+//        backRight.setPower(backRightPower);
+//    }
+//
+//    public void stop() {
+//        frontLeft.setPower(0);
+//        backLeft.setPower(0);
+//        frontRight.setPower(0);
+//        backRight.setPower(0);
+//    }
+//}
