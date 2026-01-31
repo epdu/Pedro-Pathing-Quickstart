aea54906b6d2507e0755c2b60937d08c361d3244
epdu <epdu@temple.edu>
2026-01-31 04:00:52 -0500

    auto red 3 -45-65 far

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Red3farTurret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Red3farTurret.java
@@ -124,7 +124,7 @@ public class Red3farTurret extends LinearOpMode {
         END
     }
     private final Pose startPose = new Pose(96,6.25, Math.toRadians(0)); // Start Pose further zone of our robot.
-    private final Pose shootPose = new Pose(96, 10.25, Math.toRadians(45)); // Scoring Pose of our robot. It is facing the goal at a 115 degree angle.
+    private final Pose shootPose = new Pose(96, 10.25, Math.toRadians(65)); // Scoring Pose of our robot. It is facing the goal at a 115 degree angle.
 //    private final Pose scoreEnd = new Pose(92, 92.25, Math.toRadians(0)); // Scoring Pose of our robot. It is facing the goal at a 115 degree angle.
 //    private final Pose readyFirstPickupPose = new Pose(92, 86.25, Math.toRadians(0)); // PPG  Highest (First Set) of Artifacts from the Spike Mark.
 //    private final Pose readySecondPickupPose = new Pose(92, 62.25, Math.toRadians(0)); // PGP Middle (Second Set) of Artifacts from the Spike Mark.
