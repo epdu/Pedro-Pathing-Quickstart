b252aaecb7c78b490db727e9e8c02a3dd8c32601
epdu <epdu@temple.edu>
2026-02-02 11:07:41 -0500

    qual edits

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Red3farTurret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Red3farTurret.java
@@ -25,6 +25,7 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 // 姿态
 // Limelight
 // 位姿
+
 @Autonomous(name = "RED Far THREE Southeastern Pennsylvania short try turret V1 Red9short")
 //three preload and take two roles, parking
 //shooting a little bit too long
