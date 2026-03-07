74da6e41bfd4b280a3b03c6d1bdebf117509fd62
epdu <epdu@temple.edu>
2026-03-07 00:55:45 -0500

    tune PIDF

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/TurretConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/TurretConstants.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
 public class TurretConstants {
-    public static double kP = 0.041;
+    public static double kP = 0.040;
     public static double kI = 0.0;
     public static double kD = 0.00;
     public static double kF = 0.148;
